ae8bcb9851bc835f386278138a01433efc353fd2
emilozano425 <emilozano425@gmail.com>
2022-01-19 12:27:23 -0300

    agregando api tarjetas

---
+++ TarjetaCredito/Tarjeta_Credito/AplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tarjeta_Credito.Models;
+
+namespace Tarjeta_Credito
+{
+    public class AplicationDbContext : DbContext
+    {
+
+        public  DbSet<TarjetaCredito> Tarjeta_Credito { get; set; }
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options):base(options)
+        {
+
+        }
+    }
+}
---
+++ TarjetaCredito/Tarjeta_Credito/Migrations/20220118180723_v1.0.0.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tarjeta_Credito.Migrations
+{
+    public partial class v100 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tarjeta_Credito",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titular = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumeroTarjeta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaExpiracion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cvv = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarjeta_Credito", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarjeta_Credito");
+        }
+    }
+}
